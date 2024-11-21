982e17c83a58f2a80ad3308f0f3c6072b74f0737
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-11-21 23:16:05 +0100

    add input component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,23 +24,20 @@ const formSchema = z.object({
 
 type FormType = "sign-in" | "sign-up"
 const AuthForm = ({type} : { type : FormType}) => {
-    // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
     },
-  })
+  });
  
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
    return (
+    <>
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
         <FormField
           control={form.control}
           name="username"
@@ -60,6 +57,8 @@ const AuthForm = ({type} : { type : FormType}) => {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
+    {/* OTP VERIFICATION */}
+    </>
   );
 };
 
