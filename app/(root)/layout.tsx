45ffa744cf98eb2c222eb6dee3b3986783f1757b
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-09 22:58:10 +0100

    add layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const layout = ({children} : {children: React.ReactNode}) => {
+  return ( 
+  <main className="flex h-screen">
+    Sidebar
+    <section className="flex h-full flex-1 flex-col">
+        MobileNavigation Header
+        <div className="main-content">
+            {children}
+        </div>
+    </section>
+  </main>
+  );
+};
+
+export default layout;
