3a785842c69fa34443bdd66d89b20db1769afb6c
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-10 21:50:28 +0100

    complete Header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const layout = ({children} : {children: React.ReactNode}) => {
   return ( 
   <main className="flex h-screen">
-    Sidebar
+    <Sidebar />
     <section className="flex h-full flex-1 flex-col">
-        MobileNavigation Header
+        <MobileNavigation/> 
+        <Header/>
         <div className="main-content">
             {children}
         </div>
