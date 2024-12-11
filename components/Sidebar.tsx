d314fd18af03977a4ac54a4cbc587308ce8fbd08
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-11 22:54:26 +0100

    start sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <aside className="sidebar">
+    <Link href="/">
+    <Image
+    src="/assets/icons/logo-full-brand.svg"
+    alt="logo"
+    width={160}
+    height={50}
+    className="hidden h-auto lg:block" />
+
+    <Image
+    src="/assets/icons/logo-full-brand.svg"
+    alt="logo"
+    width={52}
+    height={52}
+    className="lg:hidden" />
+    </Link>
+  </aside>;
 };
 
 export default Sidebar;
