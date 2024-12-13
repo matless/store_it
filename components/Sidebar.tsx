2624610d4fa3430a536fdca2e7032c259c0a3c98
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-13 19:57:21 +0100

    Sidebar work

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { navItems } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
+
+  const pathname = usePathname();
   return <aside className="sidebar">
     <Link href="/">
     <Image
@@ -22,7 +28,9 @@ const Sidebar = () => {
 
     <nav className="sidebar-nav">
       <ul className="flex flex-1 flex-col gap-6">
-
+        {navItems.map(({url,name, icon}) => (
+          <Link href={url}></Link>
+        ))}
       </ul>
     </nav>
   </aside>;
