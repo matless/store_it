14295fa6fa20f780bb24cda5e229ae27aa5b3939
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-16 21:05:15 +0100

    fixing bugs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,6 @@ import MobileNavigation from "@/components/MobileNavigation";
 import Sidebar from "@/components/Sidebar";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
-import React from "react";
 
 const layout =  async ({children} : {children: React.ReactNode}) => {
   const currentUser = await getCurrentUser();
