7311bfd41ebaab270e700679d180fce761cc4323
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-18 21:33:56 +0100

    .

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -5,45 +5,44 @@ import { appwriteConfig } from "./config";
 import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
-    const client = new Client()
+  const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
-    .setProject(appwriteConfig.projectId)
-    .setKey(appwriteConfig.secretKey);
+    .setProject(appwriteConfig.projectId);
 
-    const session = (await cookies()).get("appwrite-session");
-    if (!session || !session.value) throw new Error("No session")
-        client.setSession(session.value);
+  const session = (await cookies()).get("appwrite-session");
 
-    return{
-        get account() {
-            return new Account(client);
+  if (!session || !session.value) throw new Error("No session");
 
-        },
-        get databases() {
-            return new Databases(client);
-        },
-    };
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
 };
 
 export const createAdminClient = async () => {
-    const client = new Client()
+  const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
     .setProject(appwriteConfig.projectId)
     .setKey(appwriteConfig.secretKey);
 
-    return{
-        get account() {
-            return new Account(client);
-
-        },
-        get databases() {
-            return new Databases(client);
-        },
-        get storage() {
-            return new Storage(client);
-        },
-        get avatars() {
-            return new Avatars(client);
-        },
-    };
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
 };
