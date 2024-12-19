edc25b2edd4cde39cd07f3e9299d96212052bab6
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-19 22:41:39 +0100

    add shadcn sheet

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 const MobileNavigation = () => {
   return <div>MobileNavigation</div>;
