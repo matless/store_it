d61bc0c63e250d08dc5f1100e9a3575c0f613c93
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-20 13:39:59 +0100

    add sheetComponents

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -6,9 +6,26 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import Image from "next/image";
 
 const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+  return <header className="mobile-header">
+    <Image src="/assets/icons/logo/-full-brand.svg" alt="logo" width={120} height={52} className="h-auto" />
+
+    <Sheet>
+  <SheetTrigger>Open</SheetTrigger>
+  <SheetContent>
+    <SheetHeader>
+      <SheetTitle>Are you absolutely sure?</SheetTitle>
+      <SheetDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+
+  </header>
 };
 
 export default MobileNavigation;
