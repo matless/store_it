064e1591fac34029af479bcf50f05b23a33ef4e3
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-12-28 23:59:14 +0100

    signoutuser

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -15,6 +15,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
   $id: string;
@@ -105,7 +106,7 @@ const MobileNavigation = ({
             <Button
               type="submit"
               className="mobile-sign-out-button"
-              onClick={() => {}}
+              onClick={ async () => await signOutUser()}
             >
               <Image
                 src="/assets/icons/logout.svg"
--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB",
+    },
+  },
   images: {
-      remotePatterns : [
-        {
-          protocol: "https",
-          hostname: "img.freepik.com"
-        },
-        {
-          protocol: "https",
-          hostname: "cloud.appwrite.io"
-        },
-      ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ],
   },
 };
 
