fe20ea21d22ca6b6e366d296e34c124f7875802b
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-01-01 20:27:14 +0100

    add drag n drop hook to fileuploader

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+
+import React, {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
+
+
+  
+
 
 const FileUploader = () => {
-  return <div>FileUploader</div>;
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
 };
 
 export default FileUploader;
