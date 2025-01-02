df16cf098ce12d043ad6e60f0d365dca121a240d
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-01-02 21:52:37 +0100

    add Button to file uploader

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -2,20 +2,36 @@
 
 import React, {useCallback} from 'react';
 import {useDropzone} from 'react-dropzone';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 
-  
+  interface Props {
+    ownerId: string;
+    accountId: string;
+    className?: string;
+  }
 
 
-const FileUploader = () => {
+const FileUploader = ({ownerId, accountId, className} : Props) => {
   const onDrop = useCallback(acceptedFiles => {
     // Do something with the files
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className="cursor-pointer">
       <input {...getInputProps()} />
+      <Button type="button" className={cn("uploader-button", classname)}>
+        <Image
+        src="/assets/icons/upload.svg"
+        alt="upload"
+        width={24}
+        height={24} 
+        />{" "}
+          <p>Upload</p>
+      </Button>
       {
         isDragActive ?
           <p>Drop the files here ...</p> :
