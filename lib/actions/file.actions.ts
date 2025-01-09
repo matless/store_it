9464a673a10ec1fdd3a4bcf36839b29bc1ebff56
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-01-09 23:05:56 +0100

    add bucketFile

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,10 +1,34 @@
 "use server";
 
 import { UploadFileProps } from "@/types";
+import { createAdminClient } from "../appwrite";
+import { InputFile } from "node-appwrite/file";
+import { appwriteConfig } from "../appwrite/config";
+import { ID } from "node-appwrite";
+
+
+const handleError = (error: unknown, message: string) => {
+    console.log(error, message);
+    throw error;
+  };
 
 export const uploadFile = async ({
     file,
     ownerId,
     accountId,
     path,
-} : UploadFileProps) => {};
+} : UploadFileProps) => {
+    const {storage, databases} = await createAdminClient();
+
+    try {
+        const inputFile = InputFile.fromBuffer(file, file.name);
+
+        const bucketFile = await storage.createFile(
+            appwriteConfig.bucketId,
+            ID.unique(),
+            inputFile,
+        )
+    } catch (error) {
+        handleError(error, "Failed to upload file")
+    }
+};
