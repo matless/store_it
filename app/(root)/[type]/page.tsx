21162195739729fb2706f1fcd1f491cfc45fa2d3
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-01-15 22:33:52 +0100

    add sort component

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,3 +1,4 @@
+import Sort from "@/components/Sort";
 import React from "react";
 
 const Page = async ({params} : SearchParamProps) => {
@@ -9,6 +10,21 @@ const Page = async ({params} : SearchParamProps) => {
         <h1 className="h1 capitalize">
             {type}
         </h1>
+
+        <div className="total-size-section">
+            <p className="body-1">
+                Total: <span className="h5">
+                    0 MB
+
+                </span>
+            </p>
+
+            <div className="sort-container">
+                <p className="body-1 hidden sm:block text-light-200">Sort by:</p>
+                <Sort />
+            </div>
+
+        </div>
 
     </section>
    </div>
