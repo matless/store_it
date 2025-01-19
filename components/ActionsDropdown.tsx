660b8b04dd16300648924a0f6754d16743084bc2
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-01-19 22:56:43 +0100

    start work on dropdown menu

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+"use client";
+import {
+    Dialog,
+  } from "@/components/ui/dialog";
 
-const ActionsDropdown = () => {
-  return <div>ActionsDropdown</div>;
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+  
+  
+
+const ActionsDropdown = ({ file }: { file: Models.Document }) => {
+
+    const [isModalOpen, setisModalOpen] = useState(false);
+    const [isDropDownOpen, setisDropDownOpen] = useState(false);
+    
+    
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setisModalOpen}>
+  <DropdownMenu open={isDropDownOpen} onOpenChange={setisDropDownOpen}>
+  <DropdownMenuTrigger className="shad-no-focus">
+    <Image
+        src="/assets/icons/dots.svg"
+        alt="dots"
+        width={34}
+        height={34} />
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel className="max-w-[200px] truncate">
+       
+    </DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+</Dialog>
+
+  );
 };
 
 export default ActionsDropdown;
