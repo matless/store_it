da4b51b4aae6d445fb24b6d45944b0e51e2f2427
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-01-29 21:14:21 +0100

    add deleteFile function to Dropdown

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -23,7 +23,7 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
+import { deleteFile, renameFile, updateFileUsers } from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
 import { FileDetails, ShareInput } from "./ActionsModalContent";
   
@@ -58,7 +58,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
           renameFile({fileId: file.$id, name, extension: file.extension, path}),
 
         share : () => updateFileUsers({fileId: file.$id, emails, path}),
-        delete: () => console.log("delete"),
+        delete: () => deleteFile({fileId: file.$id, path, bucketFileId: file.bucketFileId}),
 
         };
 
@@ -107,6 +107,14 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
             {value === "share" && (
               <ShareInput file={file} onInputChange={setEmails} onRemove={handleRemoveUser}/>
             )}
+            {value === "delete" && (
+              <p className="delete-confirmation">
+                Are you sure you want to delete {` `}
+                <span className="delete-file-name">
+                  {file.name}
+                </span>?
+              </p>
+            ) }
 
               
           </DialogHeader>
