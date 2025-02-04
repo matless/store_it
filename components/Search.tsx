6d12ba20e97758e00ce664d1caf9268439f28f86
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-02-04 23:23:03 +0100

    add functionality to search bar

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,11 +3,28 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import { useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
 
 const Search = () => {
   const [query, setQuery] = useState("");
   const searchParams = useSearchParams();
   const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  
+  
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({searchText: query});
+        setResults(files.documents);
+        setOpen(true);
+    };
+
+    fetchFiles();
+  }, [query]);
+  
 
   useEffect(() => {
     if (!searchQuery) {
@@ -29,6 +46,22 @@ const Search = () => {
          placeholder="Search..."
          className="search-input"
          onChange={(e) => setQuery(e.target.value)} />
+
+         {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => 
+              <li key={file.$id}>
+                {file.name}
+              </li>)) :
+              (
+                <p className="empty-result">
+                    No files found
+                </p>
+              
+            )}
+          </ul>
+         )}
     </div>
   </div>
 );
