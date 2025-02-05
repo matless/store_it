84cf951ff7dbf7003badb8511b1e4de7078f0dd5
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2025-02-05 22:51:26 +0100

    add thumbnail with data to searchbar

--- components/Search.tsx
+++ components/Search.tsx
@@ -5,6 +5,8 @@ import { Input } from "./ui/input";
 import { useSearchParams } from "next/navigation";
 import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
 
 const Search = () => {
   const [query, setQuery] = useState("");
@@ -51,8 +53,19 @@ const Search = () => {
           <ul className="search-result">
             {results.length > 0 ? (
               results.map((file) => 
-              <li key={file.$id}>
-                {file.name}
+              <li className="flex items-center justify-between" key={file.$id} >
+                <div className="flex cursor-pointer items-center gap-4">
+                  <Thumbnail
+                  type={file.type}
+                  extension={file.extension}
+                  url={file.url}
+                  className="size-9 min-w-9" />
+                  <p className="subtitle-2 line-clamp-1 text-light-100">{file.name}</p>
+                </div>
+                <FormattedDateTime
+                date={file.$createdAt}
+                className="caption line-clamp-1" />
+                
               </li>)) :
               (
                 <p className="empty-result">
